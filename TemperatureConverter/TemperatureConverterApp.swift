99ad7c5b5d324e8d5f2c8c911ab60f1c02466e15
Atonbara-Adora <the.atonbara@gmail.com>
2023-01-23 21:46:21 -0800

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Atonbara Diete-Koki on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
