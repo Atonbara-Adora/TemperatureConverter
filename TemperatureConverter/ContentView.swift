35fd0024cbec5a60c5b3ad8d98cf88c9b4516c67
Atonbara-Adora <the.atonbara@gmail.com>
2023-01-23 22:11:30 -0800

    Update ContentView.swift

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -6,16 +6,64 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    var tempTypes = ["celsius", "farenheit", "kelvin"]
+    @State var inputTemp = "celsius"
+    @State var outputTemp = "celsius"
+    @State var inputValue: Double = 0.0
+    var outputValue: Double {
+        var preconvertedCelsiusVal: Double
+        
+        if inputTemp == "celsius" {
+           preconvertedCelsiusVal = Double(inputValue)
         }
-        .padding()
+        else if inputTemp == "farenheit" {
+            preconvertedCelsiusVal = Double((inputValue - 32) * 5/9)
+        }
+        else {
+            preconvertedCelsiusVal = Double(inputValue - 273.15)
+        }
+        
+        if outputTemp == "celsius" {
+            return preconvertedCelsiusVal
+        }
+        
+        else if outputTemp == "farenheit" {
+            return (preconvertedCelsiusVal * 9 / 5) + 32
+        }
+        
+        else {
+            return preconvertedCelsiusVal + 273.15
+        }
+    }
+    
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                
+                VStack {
+                    Picker("input temp unit", selection: $inputTemp){
+                        ForEach (tempTypes, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    Picker("output temp unit", selection: $outputTemp){
+                        ForEach (tempTypes, id: \.self){
+                            Text($0)
+                        }
+                    }
+                }
+                var outputValueString = String(format: "%.0f", outputValue)
+                HStack {
+                    TextField("Enter your value", value: $inputValue, format: .number)
+                    Text("Your answer is \(outputValueString)")
+                }
+                
+            }.navigationTitle("Temperature Converter")
+        }
     }
 }
 
